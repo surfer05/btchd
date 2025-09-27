a58f2423765101af6b0d8f1c04b7a207cc37c5c5
Nishkarsh Jain <69583571+NanoNish@users.noreply.github.com>
2025-09-27 02:17:57 +0000

    make dbo singleton to limit clients

--- db/walrus_dbo.py
+++ db/walrus_dbo.py
@@ -3,6 +3,7 @@ from walrus import WalrusClient
 
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
@@ -10,32 +11,39 @@ class Singleton(type):
 
 
 class DBO(metaclass=Singleton):
-    def __init__(self, publisher_url: str, aggregator_url: str):
-        self.client = WalrusClient(publisher_base_url=publisher_url, aggregator_base_url=aggregator_url)
+    def __init__(self, publisher_url: str = None, aggregator_url: str = None):
+        self.publisher_url = (
+            publisher_url or "http://walrus-publisher-testnet.haedal.xyz:9001"
+        )
+        self.aggregator_url = (
+            aggregator_url or "https://aggregator.testnet.walrus.mirai.cloud"
+        )
+        self.client = WalrusClient(
+            publisher_base_url=self.publisher_url,
+            aggregator_base_url=self.aggregator_url,
+        )
 
     def create_blob_from_data(self, data: bytes) -> str:
         response = self.client.put_blob(data=data)
-        blob_id = response.get('newlyCreated').get('blobObject').get('blobId')
+        blob_id = response.get("newlyCreated").get("blobObject").get("blobId")
         return blob_id
 
     def create_blob_from_file(self, file_path: str) -> str:
         response = self.client.put_blob_from_file(file_path)
-        blob_id = response.get('newlyCreated').get('blobObject').get('blobId')
+        blob_id = response.get("newlyCreated").get("blobObject").get("blobId")
         return blob_id
 
     def get_blob_data(self, blob_id: str) -> str:
         blob_content = self.client.get_blob(blob_id)
-        return blob_content.decode('utf-8')
-    
+        return blob_content.decode("utf-8")
+
 
 if __name__ == "__main__":
-    publisher_url = "http://walrus-publisher-testnet.haedal.xyz:9001"
-    aggregator_url = "https://aggregator.testnet.walrus.mirai.cloud"
-    client = DBO(publisher_base_url=publisher_url, aggregator_base_url=aggregator_url)
+    client = DBO()
     data = b"Hello Walrus!"
     blob_id = client.create_blob_from_data(data)
     stored_data = client.get_blob_data(blob_id)
-    if not stored_data == data.decode('utf-8'):
+    if not stored_data == data.decode("utf-8"):
         raise ValueError("Data mismatch!")
     file_path = "/workspaces/btchd/data/demoCitiesWithGeoJSON.json"
     blob_id = client.create_blob_from_file(file_path)
